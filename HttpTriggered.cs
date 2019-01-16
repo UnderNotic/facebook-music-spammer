33f62452505450b0119cd9432cabdb58a699b320
undernotic <undernotic@gmail.com>
2019-01-16 01:17:47 +0100

    get track from spotify

--- HttpTriggered.cs
+++ HttpTriggered.cs
@@ -7,27 +7,66 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Web;
 
 namespace facebook_music_spammer
 {
     public static class HttpTriggered
     {
+        private static HttpClient _httpClient = new HttpClient();
+
         [FunctionName("HttpTriggered")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var pr = "spotify:user:bambuseq:playlist:2GK6j1Wh1NfmIPcFVXC97t";
 
-            string name = req.Query["name"];
+            var content = new FormUrlEncodedContent(new[]{
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            });
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var reqB = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+            reqB.Content = content;
+            reqB.Headers.Add("Authorization", "Basic thetoken!!!");
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            var res = await _httpClient.SendAsync(reqB);
+
+            var resBody = await res.Content.ReadAsAsync<dynamic>();
+            var token = resBody["access_token"].ToString();
+            
+            log.LogInformation($"Token is {token}");
+
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["fields"] = "items(track(name,album(name))), next";
+
+            string queryString = query.ToString();
+            var reqTracks = new HttpRequestMessage(HttpMethod.Get, $"https://api.spotify.com/v1/playlists/2GK6j1Wh1NfmIPcFVXC97t/tracks?{queryString}");
+            reqTracks.Headers.Add("Authorization", $"Bearer {token}");
+
+            var ress = await _httpClient.SendAsync(reqTracks);
+            var tracks = await ress.Content.ReadAsAsync<dynamic>();
+            // var jsonTracks = JsonConvert.SerializeObject(tracks);
+            //parse this json to get only name and artist, check if limit 100 always
+
+            log.LogInformation($"Tracks are {Convert.ToString(tracks)}");
+
+            //youtube api to discover
+            //https://www.slickremix.com/docs/get-api-key-for-youtube/
+            //key thetoken!!!
+            //https://developers.google.com/youtube/v3/docs/search/list#http-request
+            //https://developers.google.com/youtube/registering_an_application#Create_API_Keys
+            //https://www.googleapis.com/youtube/v3/search?q=cat&part=snippet&key=thetoken!!!
+
+
+
+            
+            //https://www.apreche.net/tutorial-programatically-post-a-status-update-to-your-facebook-page/
+            // how to use facebook api as fb user
+
+            return (ActionResult)new OkObjectResult($"Hello");
         }
     }
 }
